4e7aaa0257576cc4e8dcbaf870352333ca13bcd2
sohaibyounas <sohaibyounas24@gmail.com>
2025-02-19 11:20:11 +0500

    newupdates

--- scriptnew.js
+++ scriptnew.js
@@ -1,12 +1,13 @@
 const navbar = document.querySelector('.navbar');
-const navbarfit = navbar.offsetTop;
+const navbarfit = navbar.PageYOffset;
 window.addEventListener('scroll', () => {
-  if (window.pageYOffset > navbarfit) {
-    navbar.classList.add('navbarfit');
-  } else {
-    navbar.classList.remove('navbarfit');
+  if(window.pageYOffset> navbarfit) {
+    navbar.addEventListener('navabrfit');
+  } else{
+    navbar.removeEventListener('navbarfit');
   }
 });;
+
 
 // change icon
 let changeIcon = function(icon) {
