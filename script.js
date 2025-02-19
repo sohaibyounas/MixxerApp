5ba1f117d1679623d1b040008567ab7e3a18280e
sohaibyounas <sohaibyounas24@gmail.com>
2025-02-19 14:06:22 +0500

    nav updates

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-//  Form Validations
-      function handleSubmit(event) {
-          event.preventDefault();
-          alert('Form submitted successfully!');
-          document.getElementById('contactForm').reset();
-      }
-
-//  navbar toggling/collapse
-    const nav = document.querySelector('.nav');
-    const sticky = nav.offsetTop;
-    window.addEventListener('scroll', () => {
-      if (window.pageYOffset >sticky) {
-        nav.classList.add('sticky');
-      } else {
-        nav.classList.remove('sticky');
-      }
-    });;
-
-const navbar = document.querySelector('.navbar');
-const navbarfit = navbar.offsetTop;
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > navbarfit) {
-    navbar.classList.add('navbarfit');
-  } else {
-    navbar.classList.remove('navbarfit');
-  }
-});;
-
 // change icon
 let changeIcon = function(icon) {
   icon.classList.toggle('fa-times')
 }
+
+//  navbar toggling/collapse
+const nav = document.querySelector('.nav');
+const sticky = nav.offsetTop;
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset >sticky) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});;
+
+const navbar = document.querySelector('.navbar');
+const navbarfit = navbar.offsetTop;
+window.addEventListener('scroll', () => {
+if (window.pageYOffset > navbarfit) {
+navbar.classList.add('navbarfit');
+} else {
+navbar.classList.remove('navbarfit');
+}
+});;
+
+// Form Validations
+function handleSubmit(event) {
+  event.preventDefault();
+  alert('Form submitted successfully!');
+  document.getElementById('contactForm').reset();
+}
+
+
+
