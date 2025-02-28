ccf31b9359f8a26195e63c3c15d2e76f6b414fa0
sohaibyounas <sohaibyounas24@gmail.com>
2025-02-28 11:35:31 +0500

    minor update

--- scriptnew.js
+++ scriptnew.js
@@ -8,18 +8,6 @@ window.addEventListener('scroll', () => {
   }
 });;
 
-//  navbar toggling/collapse
-const nav = document.querySelector('.nav');
-const sticky = nav.offsetTop;
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset >sticky) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
-});;
-
-
 // icon change
 
 let menuIcon = document.querySelector(".fbar");
